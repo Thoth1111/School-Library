bbfce071b3485226bacd0ecd507a8c7312be097a
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-04-11 09:16:33 +0300

    fix person class

--- person.rb
+++ person.rb
@@ -1,21 +1,25 @@
 class Person
-    attr_accessor :name, :age
-    attr_reader :id
-    def initialize( id, name = 'unknown', age, parent_permission = true)
-        @id = id
-        @name = name
-        @age = age 
-    end
-    def is_of_age?
-        if @age >= 18
-            return true
-        end
-        return false
-    end
-    def can_use_services?
-        if person.age >= 18 || person.parent_permission == true
-            return true
-        end
-        return false
-    end
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, name: 'unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    return true if of_age? || @parent_permission
+
+    false
+  end
+
+  private
+
+  def of_age?
+    return true if age >= 18
+
+    false
+  end
 end
