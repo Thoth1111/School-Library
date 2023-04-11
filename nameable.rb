172193aef62ee114637c4920b34c669b67964097
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-04-11 15:41:04 +0300

    Modify Person class

--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,5 @@
 class Nameable
     def correct_name
-        puts "NotImplemented"
+        puts "NotImplementedError"
     end
 end
--- person.rb
+++ person.rb
@@ -1,4 +1,6 @@
-class Person
+require_relative './nameable.rb'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
@@ -7,6 +9,10 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    return @name
   end
 
   def can_use_services?
