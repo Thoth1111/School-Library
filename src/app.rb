6a4094ac22acf3930ab5ee5d1beca0da24cf4f88
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-04-12 16:22:34 +0300

    Add app.rb and create person method

---
+++ src/app.rb
@@ -0,0 +1,37 @@
+require_relative '.person/'
+require_relative '.student/'
+require_relative '.teacher/'
+require_relative '.book/'
+require_relative './rental'
+
+class App
+  attr_accessor :persons, :books, :rentals
+
+  def initialize
+    @books = []
+    @persons = []
+    @rentals = []
+  end
+
+  def create_person
+    puts 'Do you want to create a student (1) or a teacher (2)? [input the number]:'
+    type = gets.chomp
+    puts 'Age:'
+    age = gets.chomp
+    puts 'Name:'
+    name = gets.chomp
+    if type == 1
+      puts 'Has parent permission? [y/n]:'
+      permission = gets.chomp
+      parent_permission = false if permission == n
+      person = Student.new(age, name, parent_permission)
+    elsif type == 2
+      puts 'Specialization:'
+      person = Teacher.new(age, name, specialization)
+    else
+      puts 'Invalid type selection (parent/student)'
+    end
+    @persons.push(person)
+    puts 'person created successfully'
+  end
+end
