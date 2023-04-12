689d48453a5e0a8b12912c1d1f4712cc2c600db9
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-04-12 11:09:40 +0300

    Create has-many/belongs-to relationship

--- src/student.rb
+++ src/student.rb
@@ -1,10 +1,16 @@
 require_relative './person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, classroom, name: 'unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
+
+  def classroom = (classroom)
+    @classroom = classroom
+    classroom.student.push(self) unless classroom.include?(self)
 
   def play_hookey
     '¯(ツ)/¯'
