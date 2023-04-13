5954bb8236259d92dd42be6a48b9b4b3a17691d8
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-04-13 11:41:04 +0300

    add main entry point and menu methods

--- src/main.rb
+++ src/main.rb
@@ -2,7 +2,7 @@
 require_relative './app'
 
 def main
-    puts 'Please choose an option by entering a number:\n
+  puts 'Please choose an option by entering a number:\n
     1 - List all books\n
     2 - List all people\n
     3 - Create a person\n
@@ -11,32 +11,30 @@ def main
     6 - List all rentals for a given person id\n
     7 - Exit'
 
-    app = App.new
-    selected = gets.chomp
-    if selected == '7'
-        exit
-    end
+  app = App.new
+  selected = gets.chomp
+  exit if selected == '7'
 
-    run(selected, app)
+  run(selected, app)
 end
 
 def run(selected, app)
-    case selected
-    when '1'
-        app.list_books
-    when '2'
-        app.list_persons
-    when '3'
-        app.create_person
-    when '4'
-        app.create_book
-    when '5'
-        app.create_rental
-    when '6'
-        app.person_rentals
-    else
-        puts 'Invalid input. Please try again'
-    end
+  case selected
+  when '1'
+    app.list_books
+  when '2'
+    app.list_persons
+  when '3'
+    app.create_person
+  when '4'
+    app.create_book
+  when '5'
+    app.create_rental
+  when '6'
+    app.person_rentals
+  else
+    puts 'Invalid input. Please try again'
+  end
 end
 
 main
