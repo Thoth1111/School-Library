37d2e4e648ded1fef6a90e4e29826f45fe6956f3
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-04-14 16:53:21 +0300

    buggy

--- src/rental.rb
+++ src/rental.rb
@@ -6,7 +6,7 @@ class Rental
   def initialize(date, book, person)
     @date = date
     @book = book
-    book.rentals.push(self)
+    book.rentas.push(self)
     @person = person
     person.rentals.push(self)
   end
